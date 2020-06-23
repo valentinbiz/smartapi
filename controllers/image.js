6ddb1926c6b88fe9aff959e5ce18a54948846b32
valentinbiz <valentin.bizzz@gmail.com>
2020-06-23 14:44:05 +0100

    final server

---
+++ controllers/image.js
@@ -0,0 +1,30 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: 'efdbab3a6c9e43998fec907c8dc239da'
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict('c0c0ac362b03416da06ab3fa36fb58e3', req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+    const {id} = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
